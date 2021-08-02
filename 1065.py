aa1134bf04f8917264d4a345469e9439042372ac
mjkim <joy4mj@naver.com>
2021-08-02 15:21:10 +0900

    1065번

---
+++ 1065.py
@@ -0,0 +1,11 @@
+n = int(input())
+hansu = 0
+
+for i in range(1,n+1):
+    if i < 100:
+        hansu += 1
+    else:
+        num = list(map(int, str(i)))
+        if num[2] - num[1] == num[1] - num[0]:
+            hansu += 1
+print(hansu)
